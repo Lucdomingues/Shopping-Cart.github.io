9466edbbf69e3878a4251b70d7bd643d0bde8257
Lucdomingues <lojalucc@outlook.com>
2022-08-13 13:24:56 -0300

    requisito 01

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,21 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  test('Verifica se é uma função!', () => {
+    expect(fetchProducts).toBeInstanceOf(Function);
+  });
+  test('Verifica se fetch é chamada quando recebe um parametro `computador`!', async () => {
+    expect(await fetchProducts('computador')).toBeCalledWith(fetch);
+  });
+  test('Verifica se ao receber um parametro `computador` é utilizado o endpoint correto!', async () => {
+    const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    expect(await fetchProducts('computador')).toBeCalledWith(url);
+  });
+  test('Verifica se ao receber um parametro `computador` retorna o objeto esperado!', async () => {
+    expect(await fetchProducts('computador')).toEqual(computadorSearch);
+  });
+  test('Verifica se ao receber um parametro vázio retorna uma menssagem de erro!', async () => {
+    expect(await fetchProducts()).reject.toThrow('You must provide an url');
+  });
+  // fail('Teste vazio');
 });
