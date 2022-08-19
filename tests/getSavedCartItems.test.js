40e9ee51646782add696f112ad88627637b58503
Lucdomingues <lojalucc@outlook.com>
2022-08-19 13:15:20 -0300

    requisito 07

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -5,7 +5,14 @@ localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
   test('Verifica se ao chamar a função `getSavedCartItems` `localStorage.getItem` é chamado!', () => { 
-    
+    expect.assertions(1);
+    getSavedCartItems();
+    expect(localStorage.getItem).toBeCalled();
+  });
+  test('Verifica se ao chamar a função `getSavedCartItems` `localStorage.getItem` é chamado com o parametro correto!', () => { 
+    expect.assertions(1);
+    getSavedCartItems();
+    expect(localStorage.getItem).toBeCalledWith('cartItems');
   });
   // fail('Teste vazio');
 });
